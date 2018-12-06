5aeb18ccad69c2fdb7540e98fdf5575bb71f76f3
p3t3rk1ss <kp.peter.kiss@gmail.com>
2018-12-06 14:39:48 +0100

    multi peer tests initial commit

---
+++ src/test/java/dockler/DocklerTest.java
@@ -0,0 +1,14 @@
+
+package dockler;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
+
+public class DocklerTest {
+ //   @Test
+
+
+}
